40ef37fa49110ebda4c6c34bba8b078d829f79fb
shallow11 <88030450+shallow11@users.noreply.github.com>
2022-10-18 23:37:38 +0800

    Add files via upload

---
+++ zuoye3/src/cn/itcast/session/servlet/LogoutServlet.java
@@ -0,0 +1,19 @@
+package cn.itcast.session.servlet;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+@WebServlet(name = "LogoutServlet",urlPatterns="/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws ServletException, IOException {
+        // 将Session对象中的User对象移除
+        request.getSession().removeAttribute("user");
+        response.sendRedirect("/chapter05/IndexServlet");
+    }
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response)throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
